57d82f670427b31dbf735f883003606bc895ea45
NiyathiRajeev <74712551+NiyathiRajeev@users.noreply.github.com>
2021-02-05 17:04:30 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+var database;
+var gameState = 0;
+var playerCount;
+var form, player, game;
+var canvas, backgroundImage;
+
+function setup(){
+    
+    canvas = createCanvas(500,500);
+    database = firebase.database();
+    game = new Game();
+    game.getState();
+    game.start();
+}
+
+function draw(){
+}
